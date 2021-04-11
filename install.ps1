5a088baa126de07fa0fd761cd963313462c62d79
Code$izzler <30734357+CodeSizzler@users.noreply.github.com>
2021-04-11 16:46:56 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 #Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Install Software
 choco install microsoft-edge obs-studio obs-ndi skype -y
